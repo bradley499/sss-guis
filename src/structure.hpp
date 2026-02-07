f45e65fba5d188e4130fc79b7ef2008d55d94eb3
Bradley Marshall <bradm499@gmail.com>
2026-02-07 18:41:03 +0000

    Improved exception error messages

--- src/structure.hpp
+++ src/structure.hpp
@@ -29,6 +29,10 @@ namespace sss::guis
          */
         using widget_contents_t = YAML::Node;
         /**
+         * @brief The reference to a file that declares a widget
+         */
+        using widget_file_reference_t = int;
+        /**
          * @brief Widget names and type identifiers
          */
         std::map<widget_name_t, widget_type_identifier_t> m_widgets;
@@ -37,13 +41,17 @@ namespace sss::guis
          */
         std::vector<widget_type_t> m_widget_types;
         /**
-         * @brief Collection of Widgets and their contents
+         * @brief Collection of widget names and their contents
          */
         std::map<widget_name_t, YAML::Node> m_widget_contents;
         /**
+         * @brief Collection of widget names and their declaration file
+         */
+        std::map<widget_name_t, widget_file_reference_t> m_widget_files;
+        /**
          * @brief Collection of already parsed files
          */
-        std::set<std::filesystem::path> m_parsed_files;
+        std::vector<std::filesystem::path> m_parsed_files;
         /**
          * @brief The name of this structure (only used for debug output)
          */
@@ -64,7 +72,7 @@ namespace sss::guis
          * @param type The type of the widget
          * @param contents The remaining contents of the widget
          */
-        void add_widget(widget_name_t const &name, widget_type_t const &type, widget_contents_t const &contents);
+        void add_widget(widget_name_t const &name, widget_type_t const &type, widget_contents_t const &contents, widget_file_reference_t const file_reference);
         /**
          * @brief Convert object references to numerical references
          */
